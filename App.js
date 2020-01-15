d5e59e0b675b9e0ae6c69d947953a72de6f6805b
djr7no <ryanmagill97@gmail.com>
2020-01-14 20:45:12 -0600

    Added timer function, render question function, startQuiz function and applied HTML

---
+++ App.js
@@ -0,0 +1,102 @@
+//From scratch, build a timer-based quiz application that stores high scores client-side.
+
+//other javascript:
+//<script type="text/javascript" src="Question.js"></script>
+
+//Global Variables:
+var time = 10;
+var score = 0;
+var currentQuestion = 0;
+var answer = true;
+var trueTime;
+
+var main = document.querySelector("main");
+//document.getElementById("score").innerHTML = score;
+
+//listens for user click on li tags
+main.addEventListener("click", function(event) {
+
+    var targetedClick = event.target;
+    
+    // Only target li tags to proceed to next classroom
+    if(targetedClick.matches("li")) {
+
+        JSON.stringify(targetedClick.textContent);
+
+        console.log(targetedClick.textContent);
+        if(targetedClick.getAttribute("data-answer") == "true"){
+            score = score+1;
+            console.log(score);
+            document.getElementById("score").innerHTML = score;
+        }
+        currentQuestion++;
+        renderQuestion();
+    }
+});
+
+//renders current question and choices
+function renderQuestion(){
+    main.innerHTML = "";
+    
+    questionsList.forEach(function(item, indexQuestion){
+
+        var div = document.createElement("div");
+
+        // If item isn't current question then hide it
+        if(currentQuestion != indexQuestion) {
+            div.setAttribute("class", "hide");
+        }
+        
+        var hTitle = document.createElement("h1");
+        hTitle.textContent = "Q: " + item.title;
+
+        // Create ul
+        var ul = document.createElement("ul");
+
+        // Create li tags
+        item.choices.forEach(function(cName) {
+            var li = document.createElement("li");
+            li.textContent = cName;
+
+            if(cName === item.answer){
+                li.setAttribute("data-answer", "true");
+            }else
+            {
+                li.setAttribute("data-answer", "false");
+            }
+
+            // append li to ul tag
+            ul.append(li);
+        });
+
+        div.append(hTitle);
+
+        div.append(ul);
+
+        main.append(div);
+    });
+}
+
+//starts timer and runs the renderQuestion function
+function startQuiz(){
+    trueTime = window.setInterval(myTimer, 1000);
+    
+    renderQuestion();
+
+    if(time === 0){
+        main.setAttribute("class", "hide");
+    }
+}
+
+//timer
+function myTimer(){
+    if(time>0){
+    time--;
+    console.log(time);
+    document.getElementById("gameTime").innerHTML = time.toString();
+    }else if(time==0){
+        document.getElementById("gameTime").innerHTML = time.toString();
+         alert("Times up!");
+        window.clearInterval(trueTime);
+    }
+}
---
+++ Questions.js
@@ -0,0 +1,12 @@
+var questionsList = [
+    {
+        title: "Which is NOT a common data type?",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "test?",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    }
+];
